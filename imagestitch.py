fa1f86c697f44648e747edeedb985830b584b1b5
Patrick Marlow <patrickmarlow@vt.edu>
2022-10-26 11:03:17 -0400

    Started an image stitching alg that takes multiple images

--- imagestitch.py
+++ imagestitch.py
@@ -95,3 +95,52 @@ def imagestitch(img_1, img_2):
 #cv2.imshow('Histogram equalized', img_output)
 # plt.imshow(img_output)
 # plt.show()
+
+# TAKES IN MULTIPLE IMAGES
+
+def imagestitchmult(*imgs):
+    """Convert images to gray-scale."""
+
+    img1 = cv2.cvtColor(img_1,cv2.COLOR_RGB2GRAY)
+    # plt.imshow(img1)
+    # plt.show()
+    img2 = cv2.cvtColor(img_2,cv2.COLOR_RGB2GRAY)
+    # plt.imshow(img2)
+    # plt.show()
+
+    """Creating SIFT keypoints and desciptors."""
+
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+    bf = cv2.BFMatcher()
+
+    matches = bf.knnMatch(des1,des2, k=2)
+
+    good = []
+    for m in matches:
+        if (m[0].distance < 0.5*m[1].distance):
+            good.append(m)
+    matches = np.asarray(good)
+
+    # Allign the images
+
+    if (len(matches[:,0]) >= 4):
+        src = np.float32([ kp1[m.queryIdx].pt for m in matches[:,0] ]).reshape(-1,1,2)
+        dst = np.float32([ kp2[m.trainIdx].pt for m in matches[:,0] ]).reshape(-1,1,2)
+        H, masked = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
+    else:
+        raise AssertionError('Can’t find enough keypoints.')
+
+    """Stitch the Images."""
+
+    dst = cv2.warpPerspective(img_1,H,((img_1.shape[1] + img_2.shape[1]), img_2.shape[0])) #wraped image
+    dst[0:img_2.shape[0], 0:img_2.shape[1]] = img_2 #stitched image
+    #cv2.imwrite('output.jpg',dst)
+    #cv2.imwrite('output.jpg', cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
+    dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+    plt.imshow(dst)
+    plt.show()
+
+    return dst
